b3a4976a7c46256f54250a8134d3530646ef6061
Paras Pahwa <134779342+paraspahwa@users.noreply.github.com>
2024-03-10 17:08:40 +0000

    updated

--- Day-07/conditional-handling.py
+++ Day-07/conditional-handling.py
@@ -4,5 +4,7 @@ type = sys.argv[1]
 
 if type == "t2.micro":
     print("ok we will create a instance for you")
+elif type == "t2.medium":
+    print("it will charge you more")
 else:
-    print("your input is not t2.micro")
+    print("your input is not correct")
